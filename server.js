522c705211b68eee1137742957b59c8c40168173
ManhTTFU <tuanmanh97x@gmail.com>
2020-06-23 16:04:42 +0700

    finish project

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ mongoose.connect(
 
       app.post("/create-question", (req, res) => {
         //id - content - like - dislike - createdAt
-        // console.log(req.body);
 
         const newQuestion = {
           content: req.body.questionContent,
@@ -50,7 +49,6 @@ mongoose.connect(
               message: err.message,
             });
           } else {
-            console.log(data);
             res.status(201).json({
               success: true,
               data: {
@@ -60,46 +58,14 @@ mongoose.connect(
             });
           }
         });
-
-        // fs.readFile("data.json", { encoding: "utf-8" }, (err, data) => {
-        //   if (err) {
-        //     res.status(500).json({
-        //       success: false,
-        //       message: err.message,
-        //     });
-        //   } else {
-        //     //json
-        //     // push questions
-        //     const questions = JSON.parse(data);
-        //     questions.push(newQuestion);
-
-        //     // writefile
-
-        //     fs.writeFile("data.json", JSON.stringify(questions), (error) => {
-        //       if (error) {
-        //         res.status(500).json({
-        //           success: false,
-        //           message: err.message,
-        //         });
-        //       } else {
-        //         res.status(201).json({
-        //           success: true,
-        //           data: newQuestion,
-        //         });
-        //       }
-        //     });
-        //   }
-        // });
       });
 
       app.get("/questions/:questionID", (req, res) => {
-        // console.log(req.params);
         res.sendFile(path.resolve(__dirname, "./public/question-detail.html"));
       });
 
       app.get("/get-question-by-id/:id", (req, res) => {
         const id = req.params.id;
-        // console.log("baaaaa", id);
         QuestionModel.findById(id, (err, data) => {
           if (err) {
             res.status(500).json({
@@ -123,31 +89,6 @@ mongoose.connect(
             }
           }
         });
-        // fs.readFile("data.json", { encoding: "utf-8" }, (error, data) => {
-        //   if (error) {
-        //     res.status(500).json({
-        //       success: false,
-        //       message: error.message,
-        //     });
-        //   } else {
-        //     // console.log("dxcxczxc", data);
-        //     const questions = JSON.parse(data);
-        //     // console.log("questions", questions);
-        //     let selectedQuestion;
-        //     for (const item of questions) {
-        //       // console.log(questions);
-        //       if (item.id === Number(id)) {
-        //         selectedQuestion = item;
-        //         // console.log("selectedQuestion", selectedQuestion);
-        //         break;
-        //       }
-        //     }
-        //     res.status(200).json({
-        //       success: true,
-        //       data: selectedQuestion,
-        //     });
-        //   }
-        // });
       });
 
       app.get("/get-random-question", (req, res) => {
@@ -161,7 +102,7 @@ mongoose.connect(
             const selectedQuestion = data[0];
             res.status(200).json({
               data: {
-                ...selectedQuestion._doc,
+                ...selectedQuestion,
                 id: selectedQuestion._id,
               },
             });
@@ -170,52 +111,30 @@ mongoose.connect(
       });
 
       app.put("/vote-question", (req, res) => {
-        //read file
-
         const questionId = req.body.questionId;
         const selectedVote = req.body.selectedVote;
 
         // check exist
-        QuestionModel.findByIdAndUpdate(questionId, {
-          $inc: {
-            [selectedVote]: 1,
+        QuestionModel.findByIdAndUpdate(
+          questionId,
+          {
+            $inc: {
+              [selectedVote]: 1,
+            },
           },
-        });
-
-        //updae
-        // fs.readFile("data.json", { encoding: "utf-8" }, (error, data) => {
-        //   if (error) {
-        //     res.status(500).json({
-        //       success: false,
-        //       message: error.message,
-        //     });
-        //   } else {
-        //     const questions = JSON.parse(data);
-        //     for (const item of questions) {
-        //       if (item.id === Number(req.body.questionId)) {
-        //         if (req.body.selectedVote === "like") {
-        //           item.like += 1;
-        //         } else {
-        //           item.dislike += 1;
-        //         }
-        //         break;
-        //       }
-        //     }
-
-        //     fs.writeFile("data.json", JSON.stringify(questions), (err) => {
-        //       if (err) {
-        //         res.status(500).json({
-        //           success: false,
-        //           message: err.message,
-        //         });
-        //       } else {
-        //         res.status(500).json({
-        //           success: true,
-        //         });
-        //       }
-        //     });
-        //   }
-        // });
+          (err) => {
+            if (err) {
+              res.status(500).json({
+                success: false,
+                message: err.message,
+              });
+            } else {
+              res.status(201).json({
+                success: true,
+              });
+            }
+          }
+        );
       });
 
       app.listen(3001, (err) => {
